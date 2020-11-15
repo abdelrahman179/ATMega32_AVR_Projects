9e82acb3d6ca74ec62388fb500d53baf73197206
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-15 14:27:28 +0200

    Create main.c

---
+++ WatchDog_LED/main.c
@@ -0,0 +1,40 @@
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "DIO_Interface.h"
+#include <util/delay.h>
+
+#define WDTCR    *((volatile u8 *) 0X41)
+
+int main()
+{
+	DIO_voidSetPortDirection(PORTA , 0XFF);
+
+	// WDT On
+	SET_BIT(WDTCR , 0);
+	SET_BIT(WDTCR , 1);
+	SET_BIT(WDTCR , 2);
+
+	DIO_voidSetPinValue(PORTA , PIN0 , HIGH);
+	_delay_ms(1000);
+
+	// WDT Off "Refresh"
+	WDTCR = 0XFF;
+	WDTCR = 0X00;
+
+	DIO_voidSetPinValue(PORTA , PIN0 , LOW);
+	_delay_ms(1000);
+
+	/* without disabling the WDT the system will remain reset and the led will remain toggle */
+
+	while (1)
+	{
+
+		DIO_voidSetPinValue(PORTA , PIN1 , HIGH);
+			_delay_ms(1000);
+		DIO_voidSetPinValue(PORTA , PIN1 , LOW);
+			_delay_ms(1000);
+
+	}
+
+     return 0;
+}
